c6bb8da1b2f4d22a1b3919366dfc6da45e908dd5
ArifADS <arifads@gmail.com>
2018-11-30 16:14:14 +0100

    Initial Commit

---
+++ FormData/FormData.h
@@ -0,0 +1,19 @@
+//
+//  FormData.h
+//  FormData
+//
+//  Created by Arif De Sousa on 30/11/2018.
+//  Copyright © 2018 arifads. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FormData.
+FOUNDATION_EXPORT double FormDataVersionNumber;
+
+//! Project version string for FormData.
+FOUNDATION_EXPORT const unsigned char FormDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FormData/PublicHeader.h>
+
+
