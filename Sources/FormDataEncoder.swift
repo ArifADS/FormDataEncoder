ebc6fb9e730da31d536830375f06b0716d61df26
Arif De Sousa <arifads@gmail.com>
2019-12-13 22:34:59 +0100

    [Fix] Not adding [] to arrays

--- Sources/FormDataEncoder.swift
+++ Sources/FormDataEncoder.swift
@@ -9,7 +9,6 @@
 import Foundation
 
 public class FormDataEncoder {
-  
   let boundary: String
   
   public init(boundary: String) {
@@ -30,96 +29,132 @@ public class FormDataEncoder {
 }
 
 private class _FormDataEncoder: Encoder {
-  public var codingPath: [CodingKey] { return [] }
-  public var userInfo: [CodingUserInfoKey : Any] { return [:] }
+  public var codingPath = [CodingKey]()
+  public var userInfo = [CodingUserInfoKey : Any]()
   var datas = [Data]()
   
-  func encode(_ value: Encodable, key: String) throws {
+  func encode(_ value: Encodable, key: CodingKey) throws {
+    let key = key.stringValue + (value is [Encodable] ? "[]" : "")
+    try encode(value: value, key: key)
+  }
+  
+  func encode(value: Encodable, key: String) throws {
     switch value {
     case let file as FormFile: self.datas.append(encodeFile(name: key, file: file))
+    case let value as [Encodable]: try value.forEach { try encode(value: $0, key: key) }
     default: self.datas.append(encodeField(name: key, value: value))
     }
   }
   
   func encodeField(name: String, value: Any) -> Data {
     var body = ""
-    body += "Content-Disposition:form-data; name=\"\(name)\""
-    body += "\r\n\r\n\(value)\n"
+    body += "Content-Disposition: form-data; name=\"\(name)\""
+    body += "\r\n\r\n\(value)\r\n"
     return body.data(using: .utf8)!
   }
   
   func encodeFile(name: String, file: FormFile) -> Data {
     var body = ""
-    body += "Content-Disposition:form-data; name=\"\(name)\""
+    body += "Content-Disposition: form-data; name=\"\(name)\""
     body += "; filename=\"\(file.fileName)\"\r\n"
     body += "Content-Type: \(file.type)\r\n\r\n"
     return body.data(using: .utf8)! + file.data + "\r\n".data(using: .utf8)!
   }
   
   public func container<Key>(keyedBy type: Key.Type) -> KeyedEncodingContainer<Key> where Key : CodingKey {
-    return KeyedEncodingContainer(KeyedContainer<Key>(encoder: self))
+    return KeyedEncodingContainer(KeyedContainer<Key>(encoder: self, codingPath: codingPath))
   }
   
   public func unkeyedContainer() -> UnkeyedEncodingContainer {
-    return UnkeyedContanier(encoder: self)
+    return UnkeyedContanier(encoder: self, codingPath: codingPath)
   }
   
   public func singleValueContainer() -> SingleValueEncodingContainer {
-    return UnkeyedContanier(encoder: self)
+    return SingleContainer(encoder: self, codingPath: codingPath)
   }
 }
 
 private struct KeyedContainer<Key: CodingKey>: KeyedEncodingContainerProtocol {
-  var codingPath: [CodingKey] { return [] }
-  var encoder: _FormDataEncoder
-  
-  mutating func encodeNil(forKey key: Key) throws {}
-  
+  let encoder: _FormDataEncoder
+  let codingPath: [CodingKey]
   
   mutating func encode<T>(_ value: T, forKey key: Key) throws where T : Encodable {
-    try encoder.encode(value, key: key.stringValue)
+    encoder.codingPath.append(key)
+    if value is FormFile || value is [Encodable] {
+      try encoder.encode(value, key: key)
+    } else {
+      try value.encode(to: encoder)
+    }
   }
   
+  
   func nestedContainer<NestedKey>(keyedBy keyType: NestedKey.Type, forKey key: Key) -> KeyedEncodingContainer<NestedKey> where NestedKey : CodingKey {
+    encoder.codingPath.append(key)
     return encoder.container(keyedBy: keyType)
   }
   
   func nestedUnkeyedContainer(forKey key: Key) -> UnkeyedEncodingContainer {
+    encoder.codingPath.append(key)
     return encoder.unkeyedContainer()
   }
   
-  func superEncoder() -> Encoder {
-    return encoder
-  }
-  
-  func superEncoder(forKey key: Key) -> Encoder {
-    return encoder
-  }
+  func superEncoder() -> Encoder { encoder }
+  func superEncoder(forKey key: Key) -> Encoder { encoder }
+  mutating func encodeNil(forKey key: Key) throws { }
 }
 
-private struct UnkeyedContanier: UnkeyedEncodingContainer, SingleValueEncodingContainer {
-  var encoder: _FormDataEncoder
+private struct UnkeyedContanier: UnkeyedEncodingContainer {
+  let encoder: _FormDataEncoder
+  let codingPath: [CodingKey]
+  var count: Int = 0
   
-  var codingPath: [CodingKey] { return [] }
-  
-  var count: Int { return 0 }
-  
-  func encode<T>(_ value: T) throws where T : Encodable {
-    try encoder.encode(value, key: "")
+  mutating func encode<T>(_ value: T) throws where T : Encodable {
+    if value is FormFile {
+      try encoder.encode(value, key: codingPath.last!)
+    } else {
+      try value.encode(to: encoder)
+    }
   }
   
   func nestedContainer<NestedKey>(keyedBy keyType: NestedKey.Type) -> KeyedEncodingContainer<NestedKey> where NestedKey : CodingKey {
     return encoder.container(keyedBy: keyType)
   }
   
-  func nestedUnkeyedContainer() -> UnkeyedEncodingContainer {
-    return self
+  func nestedUnkeyedContainer() -> UnkeyedEncodingContainer { self }
+  func superEncoder() -> Encoder { encoder }
+  mutating func encodeNil() throws { }
+}
+
+
+private struct SingleContainer: SingleValueEncodingContainer {
+  let encoder: _FormDataEncoder
+  let codingPath: [CodingKey]
+  
+  func encoding(_ value: Encodable) throws {
+    let key = codingPath.last!
+    try encoder.encode(value, key: key)
   }
   
-  func superEncoder() -> Encoder {
-    return encoder
+  mutating func encode<T>(_ value: T) throws where T : Encodable {
+    if value is FormFile {
+      try encoding(value)
+    } else {
+      try value.encode(to: encoder)
+    }
   }
-  
-  func encodeNil() throws {}
-  
+  mutating func encode(_ value: Bool) throws { try encoding(value) }
+  mutating func encode(_ value: String) throws { try encoding(value) }
+  mutating func encode(_ value: Double) throws { try encoding(value) }
+  mutating func encode(_ value: Float) throws { try encoding(value) }
+  mutating func encode(_ value: Int) throws { try encoding(value) }
+  mutating func encode(_ value: Int8) throws { try encoding(value) }
+  mutating func encode(_ value: Int16) throws { try encoding(value) }
+  mutating func encode(_ value: Int32) throws { try encoding(value) }
+  mutating func encode(_ value: Int64) throws { try encoding(value) }
+  mutating func encode(_ value: UInt) throws { try encoding(value) }
+  mutating func encode(_ value: UInt8) throws { try encoding(value) }
+  mutating func encode(_ value: UInt16) throws { try encoding(value) }
+  mutating func encode(_ value: UInt32) throws { try encoding(value) }
+  mutating func encode(_ value: UInt64) throws { try encoding(value) }
+  mutating func encodeNil() throws { }
 }
